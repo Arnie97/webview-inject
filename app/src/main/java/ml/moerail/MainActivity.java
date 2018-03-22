6b7d7802a8c89e466531f397e6aee7a060d56e38
Arnie97 <arnie97@gmail.com>
2018-03-22 15:17:08 +0800

    Remove the action bar.

--- app/src/main/java/ml/moerail/MainActivity.java
+++ app/src/main/java/ml/moerail/MainActivity.java
@@ -1,12 +1,12 @@
 package ml.moerail;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.webkit.WebChromeClient;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
