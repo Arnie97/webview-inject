13df7f6c4741764fcd0f66080a39e564c6a28081
Arnie97 <arnie97@gmail.com>
2018-03-22 14:43:16 +0800

    Add the WebView control.

--- app/src/main/java/ml/moerail/MainActivity.java
+++ app/src/main/java/ml/moerail/MainActivity.java
@@ -2,11 +2,22 @@ package ml.moerail;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebChromeClient;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+
+        webView.loadUrl("https://mobile.12306.cn/weixin");
     }
 }
